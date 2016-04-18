a92625d41a0132caf25a7484a143c6cb39232417
Masato Hori <rillomas@gmail.com>
2016-04-18 22:59:28 +0900

    removed polymer completely

--- src/web/lib/services/services.dart
+++ src/web/lib/services/services.dart
@@ -6,5 +6,59 @@ import 'dart:js';
 import 'package:logging/logging.dart';
 import 'package:js_interop/channel/channel.dart' as AppEngineChan;
 
-part 'async_http_request.dart';
-part 'app_engine_server_channel.dart';
+/// Server channel for app engine
+class AppEngineServerChannel {
+
+  /// Connect to server
+  Future<AppEngineChan.Socket> connect(int roomId, {Function onOpen:null, Function onClose:null}) async {
+    var url = "http://localhost:8080/api/client";
+    Logger.root.info("Requesting client from ${url}");
+    var r = await HttpRequest.request(url, method:"POST");
+    if (!(r.readyState == HttpRequest.DONE && r.status == 200)) {
+      return null;
+    }
+    // completed normally
+    var res = r.responseText;
+    // connect to the server via channel api
+    Logger.root.info("creating channel");
+    var chan = new AppEngineChan.Channel(res);
+    Logger.root.info("opening channel socket");
+    var socket = chan.open();
+    Logger.root.info("Setting handlers to socket");
+    if (onOpen != null) {
+      socket.onopen = allowInterop(onOpen);
+    }
+    if (onClose != null) {
+      socket.onclose = allowInterop(onClose);
+    }
+    socket.onerror = allowInterop(_onError);
+    socket.onmessage = allowInterop(_onMessage);
+    return socket;
+  }
+
+  void listen(Function onMessage, Function onError) {
+    _controller.stream.listen(onMessage, onError: onError);
+  }
+
+  void _onMessage(AppEngineChan.Message message) {
+    // send a meesage to all the listeners
+    _controller.add(message.data);
+
+  }
+
+  void _onError(AppEngineChan.Error err) {
+    // print(message);
+    // send a message to all the listeners
+    _controller.addError(err);
+  }
+
+  Future<bool> sendHeartbeat(int roomId, int clientId, String mutationToken) async {
+    var r = await HttpRequest.request("", method:"POST");
+    if (!(r.readyState == HttpRequest.DONE && r.status == 200)) {
+      return false;
+    }
+    return true;
+  }
+
+  StreamController<String> _controller = new StreamController<String>.broadcast();
+}
--- src/web/web/index.dart
+++ src/web/web/index.dart
@@ -1,12 +1,60 @@
-import 'package:polymer/init.dart';
-import 'package:polymer/polymer.dart';
+import 'dart:async';
 import 'package:logging/logging.dart';
-import 'package:js_interop/main_stage.dart';
+import 'package:js_interop/services/services.dart';
+
+class Initializer {
+  void init() {
+    _channel = new AppEngineServerChannel();
+    _connectToServer();
+  }
+
+  _connectToServer() async {
+    Logger.root.info("Sending connect request");
+    var onOpen = () => Logger.root.info("channel opened");
+    var onClose = () => Logger.root.info("channel closed");
+    try {
+      var socket = await _channel.connect(0, onOpen: onOpen, onClose: onClose);
+      Logger.root.info("Received channel: ${socket}");
+
+      _channel.listen(_onMessage, _onError);
+      Logger.root.info("Client Ready");
+      // enqueue periodic heartbeat
+      _heartbeater = new Timer.periodic(HEARTBEAT_INTERVAL, _pulseHeartbeat);
+    } catch (e) {
+      Logger.root.warning("server connect failed: ${e}");
+    }
+
+ }
+
+  _pulseHeartbeat(Timer t) async {
+    try {
+      await _channel.sendHeartbeat(0, 0, "");
+    } catch (e) {
+      Logger.root.warning("send heartbeat failed: ${e}");
+    }
+  }
+
+  /// Handler for Channel messages
+  void _onMessage(String message) {
+    Logger.root.info("onmessage at channel: ${message}");
+  }
+
+  /// Handler for Channel errors
+  void _onError(var error) {
+    Logger.root.info("Channel received error: $error");
+  }
+
+  static const HEARTBEAT_INTERVAL = const Duration(minutes: 3);
+
+  AppEngineServerChannel _channel;
+  Timer _heartbeater;
+}
 
 main() async {
   Logger.root.level = Level.INFO;
   Logger.root.onRecord.listen((log) {
     print(log);
   });
-  await initPolymer();
+  var i = new Initializer();
+  i.init();
 }
